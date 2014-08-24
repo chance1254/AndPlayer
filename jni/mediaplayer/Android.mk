c37f317fb54fc4717c27df0540016fc6c5198595
Viacheslav Okolitiy <okolitiy.viacheslav@yandex.ua>
2014-08-24 16:41:08 +0300

    Start media player develop

---
+++ jni/mediaplayer/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -D__STDC_CONSTANT_MACROS
+LOCAL_LDLIBS := -llog
+
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/../ffmpeg 
+
+LOCAL_SRC_FILES += \
+	source/thread.cpp \
+	source/packetqueue.cpp \
+	source/decoder.cpp \
+	source/decoder_audio.cpp \
+	source/decoder_video.cpp
+
+LOCAL_STATIC_LIBRARIES := libavcodec libavfilter libavformat libavutil libswresample libswscale
+
+LOCAL_MODULE := libmediaplayer
+
+include $(BUILD_SHARED_LIBRARY)
